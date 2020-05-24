13f0bb62778b6e7624a0ceff1282478816cf7359
Maxence VILEO <mavileo@student.42.fr>
2020-05-24 23:06:48 +0200

    fermer fenetre croix rouge

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mavileo@student.42.fr <mavileo@student.    +#+  +:+       +#+        */
+/*   By: mavileo <mavileo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 23:12:15 by user42            #+#    #+#             */
-/*   Updated: 2020/05/24 18:24:22 by mavileo@stu      ###   ########.fr       */
+/*   Updated: 2020/05/24 23:06:05 by mavileo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		cub3d(t_stru *stru)
 	raycast(stru);
 	mlx_put_image_to_window(stru->mlx_ptr, stru->win_ptr, stru->img_ptr, 0, 0);
 	mlx_hook(stru->win_ptr, 2, 1L<<0, key_hook, stru);
-	mlx_hook(stru->win_ptr, 17, 0, exit_hook, stru);
+	mlx_hook(stru->win_ptr, 17, 1L<<17, exit_hook, stru);
 	mlx_loop(stru->mlx_ptr);
 	return (0);
 }
